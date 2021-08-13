f0f0b4647d94e82c9c55c672a9d70891f8518d8e
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-08-13 18:13:15 -0400

    Update 081321-javascript.js

--- 081321-javascript.js
+++ 081321-javascript.js
@@ -1,14 +1,34 @@
+//Functionality: lexical scoping with “this” keyword
+//the key word "this" behaves differently in arrow functions compared to a regular function.
+//Lexical Scope allows inner functions to access the scope of their outer functions, but not the other way around
+//In a nested group of functions, the inner functions have access to the variables and other resources of their parent scope.
+//the child's functions are lexically bound to the execution context of their parents.
+
 const person = {
     firstName: 'Viggo',
     lastName: 'Mortensen',
     fullName: function () {
         return `${this.firstName} ${this.lastName}`
     },
+    
+//     fullName: () => {        //"this" has nothing to do with the scope where the function is created, it has to do with how the function is executed
+//         console.log(this);  // "this" refers to Window, if we do person.fullName() which means Window.fullName() - it will be undefined
+//         return `${this.firstName} ${this.lastName}`
+//     },  
+    
     shoutName: function () {
-        setTimeout(() => {
+        setTimeout(() => { 
             //keyword 'this' in arrow functions refers to the value of 'this' when the function is created
-            console.log(this);
+            console.log(this);       //"this" refers to the person Object
             console.log(this.fullName())
         }, 3000)
+        
+//     shoutName: function () {
+//         setTimeout(function () => {      //we have to use arrow function here instead
+//             console.log(this);       // "this" refers to Window object here
+//             console.log(this.fullName())     //this.fullName is not a function - it has to do with the execution context
+//         }, 3000)        
     }
 }
+
+person.fullName() //"this" refers to the left to the '.' here is the person
