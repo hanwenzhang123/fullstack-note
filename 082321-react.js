2a3727793f34f1e376c4b2ed3bc7ea462f5cffff
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-08-23 20:31:01 -0400

    Update and rename 082321-javascript.js to 082321-react.js

---
+++ 082321-react.js
@@ -0,0 +1,12 @@
+
+https://reactjs.org/docs/hello-world.html
+https://codesandbox.io/s/new
+
+class component or functional component
+
+pros of React
+- virtual DOM
+- JSX (HTML + JS)
+- Good for dev
+- Efficient context weiching is now avoided
+- Reusability
