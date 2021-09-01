600286db8581a90473d7c4c9cdf2d380db3d520e
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-09-01 19:33:36 -0400

    Update 090121-redux.js

--- 090121-redux.js
+++ 090121-redux.js
@@ -1,1 +1,16 @@
+Lifting State Up vs Composition vs Inheritance
+//Lifting State Up
+enable children components to have better smooth communication among each other
+//Composition
+{props.children} - built-in method, pass down as property children
+//Inheritance
+not a good model to use in React
+
+
+//Redux
+a library for managing react
+component tree talks to each other through layers, local state pass down to props
+redux has a store, like a database
+
+
 
