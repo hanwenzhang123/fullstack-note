2bf131bde32d11e55bd3a130a910b832366863ee
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-09-02 23:44:31 -0400

    Update 090121-redux.js

--- 090121-redux.js
+++ 090121-redux.js
@@ -58,12 +58,12 @@ import reducer from "./reducer";
 
 import App from "./App";
 
-const store = createStore(reducer);   //create the store through createStore(), the store will be generated based on reducer
+const store = createStore(reducer);   //create the store through createStore() which imported from redux, the store will be generated based on reducer
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(      //provider to inform the whole structure, for provider layer, everything inside would be props.children
-  <Provider store={store}>
-    <App />
+  <Provider store={store}>    //provider is like passing down everything to the children
+    <App />         //store is like global state, available to all children, stroe is the values in your redux store are, like the data from the database
   </Provider>,
   rootElement
 );
