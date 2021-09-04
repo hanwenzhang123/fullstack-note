8db08f1bf0fe8d1b8b51c7203890c634b44586c0
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-09-03 22:18:46 -0400

    Update 090321-redux.js

--- 090321-redux.js
+++ 090321-redux.js
@@ -1,8 +1,120 @@
+//Middleware -  integrate all the different software systems and make them work together
+//It provides common services and capabilities to applications outside of what's offered by the operating system
+
 //Redux Middleware - apply something extra in the middle, like a middle layer
 //Redux Thunk
+
+//index.js
+import ReactDOM from "react-dom";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import reducer from "./reducer";
+
+import App from "./App";
+
+const store = createStore(reducer);
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  rootElement
+);
+
+
 import {applyMiddleware} from "redux";
 import thunk from "redux-thunk";
 const store = createStore(reducer, applyMiddleware(thunk));   //explored to the whole flow
+
+
+//App.js
+import React from "react";
+import { connect } from "react-redux"; 
+import * as counterActions from "./action";
+
+class App extends React.Component {
+  render() {
+    const { numberForApp, incHandler, decHandler } = this.props; 
+
+    return (
+      <div className="App">
+        <h3>{numberForApp}</h3> 
+        <button onClick={incHandler}>+</button> 
+        <button onClick={decHandler}>-</button> 
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => ({ 
+  numberForApp: state.counterReducer 
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  incHandler: () => dispatch({ type: "INCREMENT" }), 
+  decHandler: () => dispatch(counterActions.decAction()) 
+  request: () => dispatch({type:"REQUEST"});    //add this one new
+});
+
+const ConnectedApp = connect( 
+  mapStateToProps,
+  mapDispatchToProps, 
+)(App)
+
+export default ConnectedApp
+
+
+//action.js
+const incAction = () => {
+  return {
+    type: "INCREMENT"
+  }
+}
+const decAction = () => {
+  return {
+    type: "DECREMENT"
+  }
+}
+const requestDataFromServer = () => {   //add this one new
+}
+export {
+  incAction,
+  decAction 
+}
+//The action is going through to the reducer to analyze the action
+//if we do not use the middleware, we can only do something to the action when API server involves
+
+
+//reducer.js
+import { combineReducers } from "redux";
+
+const INIT_STATE = 1;   
+
+const counterReducer = (state = INIT_STATE, action) => { 
+  switch (action.type) { 
+    case "INCREMENT":  
+      return state + 1; 
+    case "DECREMENT":  
+      return state - 1; 
+    default:
+      return state; 
+  }
+};
+
+const requestReducer = (state = INIT_STATE, action) => {    //add this one new
+  switch (action.type) {
+    case "REQUEST":
+      return {}
+    default:
+      return state;
+  }
+};
+
+const rootReducer = combineReducers({   
+  counterReducer
+});
+export default rootReducer;
 
 
 //Redux Reselector
