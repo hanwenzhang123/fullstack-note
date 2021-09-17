97d5c0e74dfae53275acc78044e67ca9369e969f
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-09-16 21:06:08 -0400

    Create 091621-mock.js

---
+++ 091621-mock.js
@@ -0,0 +1,65 @@
+for (var i = 0; i < 3; i++) {
+  setTimeout(() => console.log(i));
+}
+console.log(i);
+
+//3
+//3
+//3
+//3
+
+
+for (let i = 0; i < 3; i++) {
+  setTimeout(() => console.log(i)); 
+}
+console.log(i);
+
+//ReferenceError: i is not defined
+
+
+for (var i = 0; i < 3; i++) {
+    let ii = i
+  setTimeout(() => console.log(ii));
+}
+console.log(i);
+
+//3
+//0
+//1
+//2
+
+
+const creator = () => {
+    return function (num) {
+        return num +5
+    };
+}
+
+const addFive = creator()   //return a function
+// addFive(3) => 8
+console.log(addFive(3));
+//8
+
+// input string -> "abcabca"
+// output string -> "a"
+
+const string = "abcabca";
+
+const input = (str) => {
+    // { "a": 3, "b": 2, "c": 2 }
+    let obj = {};
+
+    for (let i=0; i<str.length; i++){
+        if (obj.hasOwnProperty(str[i])) {
+            obj[str[i]] += 1;
+        } else {
+            obj[str[i]] = 1;
+        }
+    }
+    console.log(obj);
+
+    
+}
+
+input(string)
+ 
