3cea9de9bfb39e6f9dd0c34a118456e3c8797e68
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-09-21 15:33:59 -0400

    Create 092121-interview-sitin.js

---
+++ 092121-interview-sitin.js
@@ -0,0 +1,12 @@
+proficient in component library
+learn about you and the past project you have worked on
+
+focus on frontend, but I am not resticting myself, I am open to different technology
+
+project you took a lead on and what you have learned. 
+manegment system, large table to choose time interval, initially table component
+
+testing
+- using testing library react to test the unit testing in react
+
+integrating API, front end to back end communication
