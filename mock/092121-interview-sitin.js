747ab06150a8d7860ce700194101ec1feabcbcf9
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-09-23 18:03:37 -0400

    Update and rename 092121-interview-sitin.js to mock/092121-interview-sitin.js

--- mock/092121-interview-sitin.js
+++ mock/092121-interview-sitin.js
@@ -4,7 +4,7 @@ learn about you and the past project you have worked on
 focus on frontend, but I am not resticting myself, I am open to different technology
 
 project you took a lead on and what you have learned. 
-manegment system, large table to choose time interval, initially table component
+management system, large table to choose time interval, initially table component
 
 testing
 - using testing library react to test the unit testing in react
