715c9c78dcfeedb071b7d090288b3341fc53753a
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-09-24 16:37:09 -0400

    Update 092321-mock.js

--- mock/092321-mock.js
+++ mock/092321-mock.js
@@ -90,3 +90,69 @@ class App extends React.Component {
 
 export default App;
   
+
+//Example 3 
+import React from "react";
+import "./styles.css";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      countryInfo: "",
+      numberInput: "",
+      areaCode: {
+        China: "+86",
+        USA: "+1",
+        Japan: "+81"
+      }
+    };
+  }
+
+  changeNumber = (event) => {
+    this.setState({ numberInput: event.target.value });
+  };
+
+  changeDropDown = (event) => {
+    const input = event.target.value;
+    let sliceNumberInput = this.state.numberInput;
+
+    if (this.state.numberInput[0] === "+") {
+      const areaCode = this.state.areaCode[this.state.countryInfo];
+      sliceNumberInput = sliceNumberInput.slice(areaCode.length);
+    }
+
+    this.setState((prevState) => {
+      return {
+        numberInput: prevState.areaCode[input] + sliceNumberInput,
+        countryInfo: input
+      };
+    });
+  };
+
+  render() {
+    const defaultOption = "***Select a Country***"
+    return (
+      <div className="App">
+        <h1>
+          {this.state.countryInfo} {this.state.numberInput}
+        </h1>
+        <select value={this.state.countryInfo || defaultOption} onChange={this.changeDropDown}>
+          <option disabled hidden>{defaultOption}</option>
+          <option value="China">China</option>
+          <option value="USA">USA</option>
+          <option value="Japan">JAPAN</option>
+        </select>
+        <input
+          type="text"
+          placeholder="Enter a Number"
+          value={this.state.numberInput}
+          onChange={this.changeNumber}
+        />
+      </div>
+    );
+  }
+}
+
+export default App;
+ 
