4b2a9cc0f6ecd43527352ddc93d39c91deb2f10c
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-10-02 12:35:54 -0400

    Update 093021-apple.js

--- mock/093021-apple.js
+++ mock/093021-apple.js
@@ -1,25 +1,148 @@
 What’s your most confident technique or language skill
-Which version of react
+  - JavaScript and its library 
+
+Which version of react?
 When did you start using react? What was the version of react?
+  - I started React 15.3 - React.PureComponent (7/30/2016)
+  - React 16.8 - hooks (2/16/2019)
+  - React 18 - newest - concurrent rendering (Concurrency is the ability to execute multiple tasks simultaneously)
+    - Automatic state batching
+    - Transition APIs/Suspense API
+    - Streaming server renderer
+
 Most recent project, describe the feature/component you’ve been working on
+
 How did you do mapping?? 
+  
 When you hover, where does the data come from?
+
 Do you know how to use …? Kind of data manipulation tool
+
 What do you do to ensure the cross browser functionality
+
 Last most class component you have checked for cross browser compatibility
+
 Anything you want to emphasize on your Zillow project? Why do you leave?
+  
 Talk about your dropbox project？
+
 Any experience with React Native? Did you use in the dropbox project?
+  
 Detail about dropbox project
+
 What is react memo? How does it speed up the process? When you call react memo, does the result store somewhere in the browser? 
+  
 Anything you want to share working with dropbox?
+  
+  
 What did you do at Nike?
+  
 What do you know about lazy loading. Give me an example, when did you use it on Nike.com. How did you implement it? 
+  
 What is OOP vs functional programming in JS
+
 Example when did you use OOP and when did you use functional programming
+
 What is prototype?
+  
 What is promise?
+  
 Why do we need Redux?
-If you start a small project, do you use redux or not.
-What happen when you call setState, why the page rerender? What’s the underlying logic of setState?
+  
+If you start a small project, do you use redux or not?
+  - No, Redux is recommended for large scale applications
+
+What happen when you call setState, why the page rerender? 
+  
+What’s the underlying logic of setState?
+  
 Element vs component?
+
+
+  
+Use Vue.js?
+Lifecycle of react and vue.js
+Roles had worked in different teams, solution to some problems that encountered in projects
+Only use webpack?
+New feature of ES7, use sass?
+Talk about your experience/solution to deal with different tools without just using webpack.
+Talk about web socket
+Ownership of the whole project????
+Timezone, Comfortable to work with global workers?
+
+  Sass experience?
+How is the team and responsibility of everyone
+Experience of delivering new feature and service
+How to commit, integration and deploy
+Who design and manage the build pipeline
+How collaborate and avoid duplicate work
+ 
+Update Only What’s Needed -> only update the needed observable
+Eliminate Duplicate Rendering with key in list
+Virtual Scrolling
+
+
+element means the hTML native element, like div img
+ 
+component is React word that means a react component that can have state and be reusable
+ 
+answer related the virtual dom 
+ 
+mongo db is nosql database
+sql is relational database,
+ 
+sql is more for structural data model
+ 
+nosql data is more for a flex data model
+ 
+?use Auto CI/CD tool or platform to auto deploy  the library, like Jenkins / github actions
+ 
+We have slack channel, every time new version is published, we have the notifuication
+Then, on the main app, we can use `yarn add`, it will always pull the latest
+ 
+ 
+ 
+ 
+cookie is set by server, frontend basely needs to touch cookie, local storage is set by frontnend
+ 
+ 
+SSR -> use next.js -> prebuilt the page in server
+ 
+useCallback is to cache the function in functional component with a deps array
+ 
+ 
+With bubbling, the event is first captured and handled by the innermost element and then propagated to outer elements.
+With capturing, the event is first captured by the outermost element and propagated to the inner elements
+ 
+ 
+ truthy value is a value that is considered true when encountered in a Boolean context. All values are truthy unless they are defined as falsy
+(i.e., except for false, 0, -0, 0n, "", null, undefined, and NaN).
+ 
+ 
+Foreach , map
+forEach() 
+- Iterates through the elements in an array.
+- Executes a callback for each element.
+    	- Does not return a value.
+ 
+	map()
+    - Iterates through the elements in an array.
+    - Calls the function on each element.
+    - Creating a new array as a result.
+ 
+hook method manage state in fn comp   
+Functional component + useEffect = class component (state & lifecycle)
+useEffect = componentDidMount + componentDidupdate + componentWillUnmount
+ 
+Single source of truth. 
+	The state of your whole application is stored in an object tree within a single store. ...
+State is read-only. 
+	The only way to change the state is to emit an action, an object describing what happened. ...
+ 
+ 
+
+  How do you think about separating things into components?
+How do you design component
+
+  shallow compare vs deep compare
+  DBs mongodb vs mysql(ie.performance)
