7c4d69176bf774e6a85ca6adec51332646cab4ed
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-10-02 12:01:59 -0400

    Update 093021-apple.js

--- mock/093021-apple.js
+++ mock/093021-apple.js
@@ -1,1 +1,25 @@
-
+What’s your most confident technique or language skill
+Which version of react
+When did you start using react? What was the version of react?
+Most recent project, describe the feature/component you’ve been working on
+How did you do mapping?? 
+When you hover, where does the data come from?
+Do you know how to use …? Kind of data manipulation tool
+What do you do to ensure the cross browser functionality
+Last most class component you have checked for cross browser compatibility
+Anything you want to emphasize on your Zillow project? Why do you leave?
+Talk about your dropbox project？
+Any experience with React Native? Did you use in the dropbox project?
+Detail about dropbox project
+What is react memo? How does it speed up the process? When you call react memo, does the result store somewhere in the browser? 
+Anything you want to share working with dropbox?
+What did you do at Nike?
+What do you know about lazy loading. Give me an example, when did you use it on Nike.com. How did you implement it? 
+What is OOP vs functional programming in JS
+Example when did you use OOP and when did you use functional programming
+What is prototype?
+What is promise?
+Why do we need Redux?
+If you start a small project, do you use redux or not.
+What happen when you call setState, why the page rerender? What’s the underlying logic of setState?
+Element vs component?
