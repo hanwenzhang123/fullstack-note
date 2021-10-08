8f0d73e226846df21c08693459ddeafbf3554e56
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-10-07 20:05:15 -0400

    Update 100721-apple.js

--- mock/100721-apple.js
+++ mock/100721-apple.js
@@ -1,4 +1,6 @@
 //Question 1
+//convert a counter from class component to functional component
+
 const Counter = () => {
     const [count, setCount] = useState(0);
     const incrementCount = () => {
@@ -14,6 +16,8 @@ const Counter = () => {
 
 
 //Question 2
+//implement a divide function
+
 function divide(a,b) {
 
 	let count = 0;
@@ -31,6 +35,8 @@ assignment.log(divide(12,3));
 
 
 //Question 3
+//using useEffect()
+
 import {useState, useEffect} from "react";
 
 const Counter = () => {
@@ -50,3 +56,17 @@ const Counter = () => {
 }
 
 export default Counter;
+
+
+Production build vs dev build
+Why production build
+What does webpack do
+How to improve react app performance
+How to do responsive design
+How to find and fix the layout bugs for responsive design for different devices
+How to test App and get test coverage
+How to test production build and fix bugs on production build
+Test CSS styles
+Test APIs
+Front-end API errors handling
+MongoDB? NodeJS? Backend?
