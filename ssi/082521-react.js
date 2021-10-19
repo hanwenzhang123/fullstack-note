f5cae51edc93ae2def5560e8e34e3a9e3acea012
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-10-19 15:22:32 -0400

    Add files via upload

---
+++ ssi/082521-react.js
@@ -0,0 +1,327 @@
+// Pros of React
+Easy to learn -> Strong communityt supporting
+VirtualDOM - Real DOM manipulation is very expensive
+diffing algorithms -> reconciliation
+Component-based framework -> Reusability
+JSX (HTML + JS)
+   - good for dev
+   - efficient context switching is now avoid
+
+// ES6 new feature <-> React?
+// bind, arrow func
+
+  
+import React, { PureComponent, memo } from 'react';
+import "./styles.css";
+
+// class component or functional component
+// class component -> has its own local state and lifecycle
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      
+    }
+  }
+  render() {
+    return {
+       <div className ="App">
+         <h1>Happy Wednesday</h1>
+       </div>
+     )
+  }
+}
+   
+//functional component -> does not above
+//React 16.8 -> React Hooks
+//use local state and lifecycle in functional component
+ function App() {       //const App = () => {   arrow function style also works
+   return (
+     <div className ="App">
+       <h1>Happy Wednesday</h1>
+     </div>
+     );
+ }
+
+export default App;
+ 
+
+// Lifecycle (3 phases) - mounting, updating, unmounting
+// mounting - initialize stuffs in the state in the constructor that we have over the initial render, then we call componentDidmount
+// normal function
+// componentDidmount -> API fetching asych (only once after the initial render)
+// componentDidUpdate -> config update, changing flag for next render
+// componentWillUnmount -> clean-up (removeEventLIStener removeSettimeout)
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      date: "wed"
+    }
+  }
+   componentDidMount() {         //after the initial render then componentDidMount
+      console.log("YEA I am alive")    //we can do API fetching here, like .then() .setState({data}) etc. 
+   }
+   componentDidUpdate() {        //we need to change some state to trigger the re-render, you do NOT set this.setState() here
+      console.log("Setting some value that is irrelevant to the page")  //does not influence what we see on the page
+   }
+   componentWillUnmount() {   //when you kill the component, proper clean up
+      console.log("BYE")
+   }
+  render() {
+     const {date} = this.state
+    return {
+       <div className ="App">
+         <h1>Happy Wednesday {date}</h1>      //faster than "YEA I am alive"
+         <button onClick={() => this.setState({ date: "new" })}>CCC</button>  
+       </div>
+     )
+  }
+}
+
+
+// state & props - mutable? => both immutable, read only
+// state is an object internally captured by class (in the constructor, this.state)
+// props down, parent talks to child
+// ?? whether child talks back to parent using props too? NO
+// ->using callback 
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {         //here is the state, better not to change the old state directly
+                  //setState() triggers render(), state changing is asynchronous in React
+    }    //Never mutate this.state directly, as calling setState() afterwards may replace the mutation you made. Treat this.state as if it were immutable.
+   //we would never do things like this.state.date = xxx, state is immutable, unchangeable, read only
+  } //this is against the react convention and this will NOT trigger the rerender to update the UI simultaneously.
+  handleClick = () => {      //setState modify this.state with its built-in logic and triggers the re-render after the state updates. 
+      this.setState({number: this.state.number + 1});
+   }
+  render() {
+    const dateProp = "wed";  //props, passing down
+    return {
+       <div className ="App">
+         <Title date={dateProp}/>     //passing a value dateProp to the child component <Title> (customized component)
+         <button onClick={this.handleClick}>CCC</button>  
+       </div>
+     )
+  }
+}                              //only parent changes props, only the parent component perform modification over the props
+function Title(props) {      //props means whatever in <Title date={dateProp}/>, we treat props as read only, not changing, read only
+   return (                //child's purpose is to present whatever passing down from the parent, not to change it
+     <div>
+       <h1>Happy { props.date }</h1>      //dateProp is the value, but we need the name here which is date
+     </div>
+   );
+ }
+
+//destructuring
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {  
+      number: 0
+    }
+  }
+   handleClick = () => {
+      const {number} = this.state;
+      this.setState({number: number + 1});
+   }
+  render() {
+    const {number} = this.state;    //this.state.number
+    return {
+       <div className ="App">
+         <Title date={number}/>     //since destructuring, here we do {number} is good
+         <button onClick={this.handleClick}>CCC</button>  
+       </div>
+     )
+  }
+}   
+function Title(props) {
+   const {date} = props;      //propss.date
+   return (
+     <div>
+       <h1>Happy { date }</h1>  
+     </div>
+   );
+ }
+ 
+ 
+// React.PureComponent -> performance improvement
+// with PureComponent, it already contains the logics of shouldComponentUpdate  - compare the props
+// to compare current props and previous props to make sure it cuts off unnecessary renders
+
+// class wrap with purecomppnent
+// function wrap with memo
+ 
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {  
+      number: 0
+    }
+  }
+   handleClick = () => {
+      const {number} = this.state;
+      this.setState({number: number + 1});
+   }
+  render() {
+    const {number} = this.state;
+    return (
+       <div className ="App">
+         <Title /> 
+         <h3> {number} </h3>
+         <button onClick={this.handleClick}>CCC</button>  
+       </div>
+     )
+  }
+} 
+class Title extends React.PureComponent {  //extends React.PureComponent, always compare the previous props and current props to determine if needs re-render
+//   constructor(props) {
+//     super(props);
+//   }  
+//   shouldComponentUpdate() {...}    //PureComponent works like containing logics with shouldComponentUpdate - compare the props to see if any changes
+  render() {  
+     console.log("Title rendering");    //this title will only render once, considers shouldComponentUpdate, we cut out unnecessary rendering
+     return (
+      <div>
+       <h1>Happy Today</h1>  
+     </div>
+    );
+  }
+}
+export default App;
+
+
+//using memo for functional component, capitalize the first letter for customized component
+
+function Title() {
+  console.log("Title rendering");    //only render once, considers shouldComponentUpdate
+  return (
+     <div>
+       <h1>Happy Today</h1>  
+     </div>
+   );
+}
+const WrapperTitle = memo(Title);      //using memo and change return to the <WrapperTitle /> 
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {  
+      number: 0
+    }
+  }
+   handleClick = () => {
+      const {number} = this.state;
+      this.setState({number: number + 1});
+   }
+  render() {
+    const {number} = this.state;
+    return (
+       <div className ="App">
+         <WrapperTitle />     //here we use <WrapperTitle /> 
+         <h3> {number} </h3>
+         <button onClick={this.handleClick}>CCC</button>  
+       </div>
+     )
+  }
+} 
+
+
+// HOC -> High Order Component
+// is not a component; it is a function (patttern);
+// a pattern where a function takes a component as an argument and returns a new component
+// take in the original component, and add some decoration and modification and props to make it a new component, add more contents
+// why HOC? use it for reusability
+// to share common functionality between components
+// same pattern but only applies to the one when we need it, and simply removes it when we do not need it
+
+connect(a, b)(OriginalComp) // use case example in React-Redux
+const NewComponent = someHOCLogic(App);
+<NewComponent />
+   
+const NewComponent = higherOrderComponent(originalComponent)
+const EnhancedComponent = higherOrderComponent(originalComponent)
+const IronMan = withSuit(TonyStark)
+
+import React from "react";
+
+const UpdatedComponent = (OriginalComponent) => {
+  class NewComponent extends React.Component{
+    render() {
+      return <OriginalComponent name="inject props"/>
+    }
+  }
+  return NewComponent
+}
+
+export default UpdatedComponent;
+   
+   
+//setState
+- a way to properly modify local state
+- triggers a re-render
+- when invole previous value, we should always use a callback function is properly being handled base on the current value
+//setState - Asynchronous
+//react will batch several setStates together into a single update for performing the state change due to performance
+
+function Title() {
+  console.log("Title rendering"); 
+  return (
+     <div>
+       <h1>Happy Today</h1>  
+     </div>
+   );
+}
+const WrapperTitle = memo(Title);  
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {  
+      number: 0
+    }
+  }
+   handleClick = () => {
+      const {number} = this.state;
+//       this.setState({number: number + 1});      //will only add once because setState is async
+//       this.setState({number: number + 1});      //React will batch several setState together to perform the state change
+//       this.setState({number: number + 1});      //use callback function to setState to make it correctly rendered instead of just assigning the new object
+       this.setState((prevState) => {     //passing in a callback function instead of setState directly
+         return { number: prevState.number + 1 };
+       })
+       this.setState((prevState) => {
+         return { number: prevState.number + 1 };
+       })
+   }
+  render() {
+    const {number} = this.state;
+    return {
+       <div className ="App">
+         <WrapperTitle /> 
+         <h3> {number} </h3>
+         <button onClick={this.handleClick}>Click</button>  
+       </div>
+     )
+  }
+}   
+
+ 
+//setTimeout example
+handleClick = () => {
+    const { number } = this.state;
+    setTimeout(() => {     //no good, synchronous
+      this.setState({ number: number + 1 }); // 0: 0 + 1    //always pass the copy, always the number, 0 + 1, closure case
+      this.setState({ number: number + 1 }); // 1: 0 + 1    //change back to 0 + 1, outdated value
+    }, 0)
+    V.S.
+  //const { number } = this.state;   
+    setTimeout(() => {     //correct value
+      this.setState({ number: this.state.number + 1 }); // 0: 0 + 1
+      this.setState({ number: this.state.number + 1 }); // 2: 1 + 1     //no closure, updated value
+    }, 0)
+  }
+}
---
+++ ssi/090121-redux.js
@@ -0,0 +1,159 @@
+Lifting State Up vs Composition vs Inheritance
+//Lifting State Up
+enable children components to have better smooth communication among each other
+//Composition
+{props.children} - built-in method, pass down as property children
+//Inheritance
+not a good model to use in React
+
+
+//Redux
+a library for managing react
+component tree talks to each other through layers, local state pass down to props
+redux has a store, like a database
+
+/*
+Why we use Redux?
+https://redux.js.org/
+Redux - A Predictable State Container for JS Apps
+Advantages:
+  Avoid complicated communication for large-scale application
+  Avoid excessive lifting state up in ReactJS
+  Excellent tool for time-traveling debugging
+  Better state management
+
+Three Principles:
+  Single source of truth - (store definition) - keep all data to the store
+  State is read-only, immutable, persistent data structure
+  Changes are made with pure function (reducer) -  change needs action
+
+Action:
+  An object include the action type/payload - the content you gonna use to make the change
+    What you try to do? like number increase, decrease?
+  Emit an action to reducer (disptach the action)
+
+Reducer:
+  Expecting all types of action as defined. 
+  Pure function is static, when we do not perform the render.
+  Pure function, A input -> A output a + b = c (same input with consistant output)
+  Reply on the input, and local state (state at the moment)
+  no side-effect, output will be predictable
+  
+How do you group different reducers?
+- combineReducers()
+
+Store:
+
+Redux Flow:
+User Click Btn:
+ReactJS -> setState() -> local state update -> UI re-rendering -> Ta-la
+ReactRedux -> emit an action (dispatch an action) -> Reducer will calculate next state 
+          -> Component subscribing to the store data re-rendering
+*/
+
+
+//index,js - where to setup Redux
+import ReactDOM from "react-dom";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import reducer from "./reducer";
+
+import App from "./App";
+
+const store = createStore(reducer);   //create the store through createStore() which imported from redux
+                                    //the store will be generated based on reducer that analyze behaviors and modify current local state
+const rootElement = document.getElementById("root");
+ReactDOM.render(      //provider to inform the whole structure, for provider layer, everything inside would be props.children
+  <Provider store={store}>    //provider is like passing down everything to the children, we pass our store down to every level via createStore(reducer)
+    <App />         //store is like global state, available to all children, stroe is the values in your redux store are, like the data from the database
+  </Provider>,    //initial render into our App
+  rootElement
+);
+
+
+//App.js
+import React from "react";
+// HOC - add the specific example
+import { connect } from "react-redux";      //the helper function, connect is a function
+import * as counterActions from "./action";   //import all kind of actions we have in the action file
+// actions = { .... ..... ..... }   //include everything
+
+class App extends React.Component {
+  render() {
+    // const { number } = this.state;     //we do not have it anymore
+    const { numberForApp, incHandler, decHandler } = this.props;    //get from the props, the state and dispatch we defined through connect()
+
+    return (
+      <div className="App">
+        <h3>{numberForApp}</h3>       //display
+        <button onClick={incHandler}>+</button>     //using the function as clikc handler
+        <button onClick={decHandler}>-</button>     //using the function as clikc handler
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => ({   //build the parameter from connect(), we take the state
+  numberForApp: state.counterReducer      //using the value from the store, counterReducer is for the specific one from the reducer file
+});
+
+const mapDispatchToProps = (dispatch) => ({   //build the parameter from connect(), we take the disptach, we pass the function, we disptach the action
+  // action generator, directly talking to the store, if you do not need a function, just delete it, no worries about props
+  incHandler: () => dispatch({ type: "INCREMENT" }),    //dispatch(actions.incAction()) - containing our action type (a payload)
+  decHandler: () => dispatch(counterActions.decAction())    //call the action, emit the action that we defined in the action.js
+});
+
+// ConnectedApp = connect()(App)
+// const ConnectedApp = connect(    //connect() takes 2 parameter - mapStateToProps and mapDispatchToProps
+//   mapStateToProps, // on value
+//   mapDispatchToProps, // on handler/actions
+// )(App)
+
+const ReduxHOC = connect(      //here we use the connection function, connect will create the HOC wrapper that takes the APP
+  mapStateToProps, // on value - get the value from the store, make sure the component is hook up with the store (display)
+  mapDispatchToProps // on handler/actions - the action we need (user interaction)
+);
+const ConnectedApp = ReduxHOC(App);   //here is to connect to our App, we do not connect directly
+
+export { ConnectedApp as default, App };  //export default ConnectedApp
+
+
+//action.js - define actions, action generator
+const incAction = () => {
+  return {
+    type: "INCREMENT"
+  }
+}
+const decAction = () => {   //add a new action
+  return {
+    type: "DECREMENT"
+  }
+}
+export {    //export the actions we defined
+  incAction,
+  decAction 
+}
+//The action is going through to the reducer to analyze the action
+
+
+//reducer.js - reply on the input and the local state at the moment
+import { combineReducers } from "redux";    //import combineReducers for the reducer file, group different reducers when we have a lot of reducers
+
+const INIT_STATE = 1;   //we need an init value from the store
+                                            //action will be emit from the action.js file
+const counterReducer = (state = INIT_STATE, action) => {    //state is the current state with a default value 
+  switch (action.type) {      //judged by the different types of actions
+    case "INCREMENT":       //different cases, make sure matches in the action file
+      return state + 1;       //return the current local state does what
+    case "DECREMENT":       //tell reducer, whenever you see this case, do something
+      return state - 1;       //what to do when see the case
+    default:            //always end with a default case
+      return state;    //just return itself
+  }
+};
+
+const rootReducer = combineReducers({     //using combineReducers() imported from redux, here pass the switch cases counterReducer to it, but you can pass more 
+  counterReducer
+});
+export default rootReducer;
+ 
