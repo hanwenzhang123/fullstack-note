8fc05f49909f76be44598f1f40b475f841e8a1df
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-10-21 15:18:24 -0400

    Create 102121-interview.js

---
+++ mock/102121-interview.js
@@ -0,0 +1,12 @@
+10/21
+Q1. Introduce yourself
+Q2. Explain event loop
+Q3. Getter and setter
+Q4. How to import a function from another file
+Q5. Difference between arrow function and normal function. How to choose between them.
+Q6. How to clone an object
+Q7. Given an integer array, how to print/filter only positive numbers
+Q8. Difference between forEach and filter
+Q9. Given an number array, how to deduplicate
+Q10. Use case of flex box and css grid
+Q11. Pros and cons between SVG icon and icon font
