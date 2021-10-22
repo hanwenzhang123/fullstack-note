481e3b13ef1835416221e40b67fed5a30150904c
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-10-22 13:45:35 -0400

    Update and rename 102121-javascript to 102121-javascript.js

--- training-antra/102121-javascript.js
+++ training-antra/102121-javascript.js
@@ -29,45 +29,45 @@ Key Points:
 - What is high order function
 
 
-// NODE, Javascript, EcmaScript(ES5, ES6, ES7...)
-// EcmaScript = Sytanx standard
-// Javacirpt =  EcmaScript + webAPI
-// NODE = EcmaScript + NODEAPI
+NODE, Javascript, EcmaScript(ES5, ES6, ES7...)
+EcmaScript = Sytanx standard
+Javacirpt =  EcmaScript + webAPI
+NODE = EcmaScript + NODEAPI
 
-// primative type 
-// let num =5; // number
-// console.log(typeof num)
-// let str = 'abc';
-// console.log(typeof str)
-// let b = true;
-// console.log(typeof b)
-// let und = undefined;
-// console.log(typeof und)
-// let n = null;
-// console.log(typeof n);
+primative type 
+let num =5; // number
+console.log(typeof num)
+let str = 'abc';
+console.log(typeof str)
+let b = true;
+console.log(typeof b)
+let und = undefined;
+console.log(typeof und)
+let n = null;
+console.log(typeof n);
 
-// let c = 10;
-// let d = c;
-// d = 5;
-// console.log('d',d) // 5
-// foo(c);
+let c = 10;
+let d = c;
+d = 5;
+console.log('d',d) // 5
+foo(c);
 
-// console.log("c",c) // 10
+console.log("c",c) // 10
 
-// function foo(input){
-//     input = 7;
-//     console.log("input",input) // 7
-// }
+function foo(input){
+    input = 7;
+    console.log("input",input) // 7
+}
 
-/// object data type : object | function
-// function foo(){
+/ object data type : object | function
+function foo(){
 
-// }
-// console.log(typeof foo);
-// let obj = {}
-// console.log(typeof obj);
-// let arr = []
-// console.log(typeof arr);
+}
+console.log(typeof foo);
+let obj = {}
+console.log(typeof obj);
+let arr = []
+console.log(typeof arr);
 
 let obj = {name:'patrick'}
 
@@ -80,5 +80,3 @@ function foo(input){
 
 // deepcopy, shallow copy
 // class key in ES6 and constructor function in ES5
-
-
