3ed5e37424f65658c0860282a303de11f9738282
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-10-23 23:00:16 -0400

    Update 102221-javascript.js

--- training-antra/102221-javascript.js
+++ training-antra/102221-javascript.js
@@ -1,4 +1,23 @@
-//primitiva data are immutable
+//primitiva data are immutable, they cannot be altered.
+
+
+// Using a string method doesn't mutate the string
+var bar = "baz";
+console.log(bar);               // baz
+bar.toUpperCase();
+console.log(bar);               // baz
+
+// Using an array method mutates the array
+var foo = [];
+console.log(foo);               // []
+foo.push("plugh");
+console.log(foo);               // ["plugh"]
+
+// Assignment gives the primitive a new (not a mutated) value
+bar = bar.toUpperCase();       // BAZ
+
+
+//the variable may be reassigned a new value, but the existing value can not be changed in the ways that objects, arrays, and functions can be altered.
 
 
 //reference type data you can change, function is also objects
@@ -10,3 +29,5 @@ function foo(input) {
 
 foo.a = 5;
 console.log(foo.a) //5
+
+  
