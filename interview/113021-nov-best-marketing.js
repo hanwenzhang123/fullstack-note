6509a9248c86fbfa335e19027d830e6641f93f01
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-12-04 14:35:52 -0500

    Update 113021-nov-best-marketing.js

--- interview/113021-nov-best-marketing.js
+++ interview/113021-nov-best-marketing.js
@@ -1,3 +1,11 @@
+11/1 Kevin
+Q1
+Q2
+Q3 prefer function or class component
+Q4 TypeScript exp? What do you use it for? How do you like using it?
+Q5 Event loop, setTimeout(0)?
+Q6 configuring SPA, what’s code splitting? What’s it good for?
+
 11/02 Doug
 Self intro
 More full stack backend or frontend?
@@ -10,13 +18,7 @@ Project you worked on that makes you excited, the project you enjoyed most
 Any new technologies and upcoming technologies you know about, excited about?
 How much experience with stylesheets, CSS, or libraries like bootstrap?
 
-Q3 prefer function or class component
-Q4 TypeScript exp? What do you use it for? How do you like using it?
-Q5 Event loop, setTimeout(0)?
-Q6 configuring SPA, what’s code splitting? What’s it good for?
-
-  
-  11/22 Kevin
+11/22 Kevin
 Setter and getter, why use them
 Arrow function vs regular function
 Deep clone & shallow clone
@@ -35,24 +37,4 @@ const a = {a: [1,2,3]};
 const b = copy(a);
 console.log(JSON.stringify(a) === JSON.stringify(b)); // true
 console.log(a === b); // false
-	
-11/02 Doug
-Self intro
-More full stack backend or frontend?
-Introduce your project
-Redux mechanism?
-React lifecycle hooks
-Is react the only framework you used in previous projects
-RestFUL API call vs web socket vs long polling
-Project you worked on that makes you excited, the project you enjoyed most
-Any new technologies and upcoming technologies you know about, excited about?
-How much experience with stylesheets, CSS, or libraries like bootstrap?
 
-  
-  11/1 Kevin
-Q1
-Q2
-Q3 prefer function or class component
-Q4 TypeScript exp? What do you use it for? How do you like using it?
-Q5 Event loop, setTimeout(0)?
-Q6 configuring SPA, what’s code splitting? What’s it good for?
