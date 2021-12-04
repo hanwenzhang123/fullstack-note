476eee52181ed4ab4bbe1c88b424eb18f8088fbf
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-12-04 16:33:21 -0500

    Update 091621-mock.js

--- interview/091621-mock.js
+++ interview/091621-mock.js
@@ -1,4 +1,7 @@
 //Test 1 
+//var variables are function scope.
+//It means they are only available inside the function they are created in, 
+//or if not created inside a function, they are globally scoped. 
 
 for (var i = 0; i < 3; i++) {
   setTimeout(() => console.log(i));
@@ -12,25 +15,23 @@ console.log(i);
 
 
 //Test 2
+//Variables declared using the let keyword are block-scoped, 
+//which means that they are available only in the block in which they were declared.
 
 for (let i = 0; i < 3; i++) {
-  setTimeout(() => console.log(i));   
+  setTimeout(() => console.log(i));   //0 1 2
 }
-console.log(i); 
+console.log(i); //ReferenceError: i is not defined
 
-//ReferenceError: i is not defined
-//0 
-//1 
-//2 
 
 
 //Test 3
 
 for (var i = 0; i < 3; i++) {
-    let ii = i
-  setTimeout(() => console.log(ii));
+  let ii = i
+  setTimeout(() => console.log(ii));  //0 1 2
 }
-console.log(i);
+console.log(i);   //3
 
 //3
 //0
