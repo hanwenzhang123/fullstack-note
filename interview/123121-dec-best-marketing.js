3c564e42d8eafc146e9abc4c7559141a4b6421e4
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-12-12 21:30:36 -0500

    Create 123121-dec-best-marketing.js

---
+++ interview/123121-dec-best-marketing.js
@@ -0,0 +1,29 @@
+12/10 Jianlong - Apple
+CSS:
+margin collapse
+highlight getElementByClass, nth-child
+Javascript:
+pass params to function
+filter function / how about if we want to change instead of return a new arr
+react:
+state props different
+props cannot be changed
+context or redux to pass data
+virtual dom: how it works. 
+react lazy loading
+npm start vs npm build
+deployment
+arr of component: missing a key
+any question: samples -> about project, about team, about sprint.
+
+
+
+12/03 Kevin - wells fargo
+Introduce yourself
+Talk about your current project
+Arrow function
+Tell us about Redux, thunk, saga
+Ways to improve web performance
+Coding challenge:
+1.A function taking two numbers as arguments. Return true if the sum of any digit in num1 is equal to num2. (e.x. 100365, 8 => true, because 3 + 5 = 8)
+2.Use React to implement a calendar (using December 2021 as data set)
