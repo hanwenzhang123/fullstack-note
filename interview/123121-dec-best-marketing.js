8877d1879150a8df78bf11bd3b41a498e338ef92
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-12-16 23:08:51 -0500

    Update 123121-dec-best-marketing.js

--- interview/123121-dec-best-marketing.js
+++ interview/123121-dec-best-marketing.js
@@ -1,3 +1,48 @@
+12/15 Jianlong
+project react based
+unit test coverage 90%
+project details
+	lifecycle
+react-router:
+wrapper for app, then path and link
+match path to component.
+change the state in parent, then pass as props
+Or we can do it in redux to update value
+design part:
+	design for comment patterns
+constant: primitive, array / object / reference
+apple care team. Vue.js.
+ 
+Coding part:
+Given an array of object,
+Use map, filter to get specific elements as an array.
+
+输出一个generator. (yield)
+ 
+var countPrimes = function(n) {
+    const isPrime = num => {
+        if (num <= 1) {  
+            return false
+        }
+        if (num % 2 === 0) {
+            return num === 2
+        }
+        for (let i = 3; i <= Math.sqrt(num); i += 2) { //小于等于
+            if (num % i === 0) 
+                return false
+        }
+        return true
+    }
+    let res = 
+    for (let k = 0; k < n; k++) { //这里是n不是num
+        if (isPrime(k))
+            res.push(k)
+    }
+    return res
+};
+
+
+
 12/10 Jianlong - Apple
 CSS:
 margin collapse
@@ -16,6 +61,8 @@ deployment
 arr of component: missing a key
 any question: samples -> about project, about team, about sprint.
 
+Q: comparing two objects to determine if the first one contains the same properties as the second one 
+  matchObject(obj1, obj2) //return Boolean
 
 
 12/03 Kevin - wells fargo
